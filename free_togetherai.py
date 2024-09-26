d1685ff9a5f4f070635e7af669a6d8fcb434c32a
xiaoming <lininruc@gmail.com>
2024-09-26 12:19:13 +0800

    add mvs

---
+++ free_togetherai.py
@@ -0,0 +1,18 @@
+import os,json
+from openai import OpenAI
+
+together = OpenAI(api_key=os.environ.get('TOGETHER_API_KEY'),
+                  base_url="https://api.together.xyz/v1")
+
+response = together.chat.completions.create(
+    model="meta-llama/Llama-Vision-Free",
+    messages=[
+        {"role": "user",
+          "content": [
+                      {"type": "text",
+                        "text": "Describe the image"},
+                      {"type": "image_url",
+                        "image_url": { "url": "https://photogen.uhaka.com/image/04b19fb9-78d2-4b65-b622-e9b91b32a7cf.png"}}
+                     ]
+        }])
+print(response.choices[0].message.content)
